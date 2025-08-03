47497a3c994ceb3d83fdc08ad7102d013da30245
mubarizullah <mubarizullah4834@gmail.com>
2025-08-03 07:39:48 +0500

    project more cleaned

--- Assets/Scripts/WatchVibrateFunctionality.cs
+++ Assets/Scripts/WatchVibrateFunctionality.cs
@@ -25,7 +25,12 @@ public class WatchVibrateFunctionality : MonoBehaviour
 
     }
 
-   
+    private void OnDestroy()
+    {
+        TouchCheck.OnTouch -= OnFirstConvoPanelBackClicked;
+    }
+
+
 
     //void OnWatchButtonClicked()
     //{
